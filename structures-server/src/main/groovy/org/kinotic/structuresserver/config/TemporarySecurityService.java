c95687fbbf89279798caad440f03216a72cf4179
Nic Padilla <nicholas@mindsignited.com>
2023-06-17 01:08:55 -0600

    security fix and some gitignores

--- structures-server/src/main/groovy/org/kinotic/structuresserver/config/TemporarySecurityService.java
+++ structures-server/src/main/groovy/org/kinotic/structuresserver/config/TemporarySecurityService.java
@@ -1,45 +1,24 @@
 package org.kinotic.structuresserver.config;
 
-import org.kinotic.continuum.core.api.event.EventConstants;
-import org.kinotic.continuum.core.api.security.MetadataConstants;
 import org.kinotic.continuum.core.api.security.Participant;
-import org.kinotic.continuum.core.api.security.Permissions;
 import org.kinotic.continuum.core.api.security.SecurityService;
-import org.kinotic.continuum.internal.core.api.security.DefaultParticipant;
-import reactor.core.publisher.Mono;
 
-import java.util.List;
+import javax.security.sasl.AuthenticationException;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 public class TemporarySecurityService implements SecurityService {
 
     private static final String PASSWORD = "structures";
-
-    private static final Map<String, Participant> participantMap = Map.of("admin",
-            new DefaultParticipant("admin",
-                    Map.ofEntries(MetadataConstants.USER_TYPE),
-                    new Permissions(List.of(EventConstants.SERVICE_DESTINATION_SCHEME + "://*.**",
-                            EventConstants.STREAM_DESTINATION_SCHEME + "://*.**"),
-                            List.of(EventConstants.SERVICE_DESTINATION_SCHEME + "://*.**",
-                                    EventConstants.STREAM_DESTINATION_SCHEME + "://*.**"))));
-
+    private static final Participant participant = new Participant("admin");
 
     @Override
-    public Mono<Participant> authenticate(String accessKey, String secretToken) {
-        if(participantMap.containsKey(accessKey) && secretToken.trim().equals(PASSWORD)){
-            return Mono.just(participantMap.get(accessKey));
-        }else{
-            return Mono.error(new IllegalArgumentException("Participant cannot be authenticated with information provided"));
+    public CompletableFuture<Participant> authenticate(Map<String, String> authenticationInfo) {
+        if(authenticationInfo.containsKey("login") && Objects.equals(authenticationInfo.get("login"), "admin")
+            && authenticationInfo.containsKey("passcode") && Objects.equals(authenticationInfo.get("passcode"), PASSWORD)){
+            return CompletableFuture.completedFuture(participant);
         }
+        return CompletableFuture.failedFuture(new AuthenticationException("username/password pair provided was not correct."));
     }
-
-    @Override
-    public Mono<Participant> findParticipant(String participantIdentity) {
-        if(participantMap.containsKey(participantIdentity)){
-            return Mono.just(participantMap.get(participantIdentity));
-        }else{
-            return Mono.error(new IllegalArgumentException("Participant cannot be found for the information provided"));
-        }
-    }
-
 }
