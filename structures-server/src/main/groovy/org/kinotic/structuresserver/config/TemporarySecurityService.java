8a89ea32bb7df4405bfc0e4389c4696947068304
Navid Mitchell <navid@mitchellsoftware.net>
2023-06-17 05:02:04 -0600

    Small change

--- structures-server/src/main/groovy/org/kinotic/structuresserver/config/TemporarySecurityService.java
+++ structures-server/src/main/groovy/org/kinotic/structuresserver/config/TemporarySecurityService.java
@@ -1,9 +1,12 @@
 package org.kinotic.structuresserver.config;
 
+import org.kinotic.continuum.core.api.security.DefaultParticipant;
+import org.kinotic.continuum.core.api.security.MetadataConstants;
 import org.kinotic.continuum.core.api.security.Participant;
 import org.kinotic.continuum.core.api.security.SecurityService;
 
 import javax.security.sasl.AuthenticationException;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -11,7 +14,9 @@ import java.util.concurrent.CompletableFuture;
 public class TemporarySecurityService implements SecurityService {
 
     private static final String PASSWORD = "structures";
-    private static final Participant participant = new Participant("admin");
+    private static final Participant participant = new DefaultParticipant("admin", "kinotic",
+                                                                          Map.of(MetadataConstants.TYPE_KEY, "user"),
+                                                                          List.of("ADMIN"));
 
     @Override
     public CompletableFuture<Participant> authenticate(Map<String, String> authenticationInfo) {
