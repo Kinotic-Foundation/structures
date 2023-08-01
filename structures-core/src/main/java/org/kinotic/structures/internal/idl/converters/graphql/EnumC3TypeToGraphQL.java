5b3904ef3bd02d8d882a802c46a185bb8d530288
Navid Mitchell <navid@mitchellsoftware.net>
2023-07-31 22:21:57 -0600

    Small changes to equality comparison

--- structures-core/src/main/java/org/kinotic/structures/internal/idl/converters/graphql/EnumC3TypeToGraphQL.java
+++ structures-core/src/main/java/org/kinotic/structures/internal/idl/converters/graphql/EnumC3TypeToGraphQL.java
@@ -2,6 +2,7 @@ package org.kinotic.structures.internal.idl.converters.graphql;
 
 import graphql.schema.GraphQLEnumType;
 import org.kinotic.continuum.idl.api.converter.C3ConversionContext;
+import org.kinotic.continuum.idl.api.converter.Cacheable;
 import org.kinotic.continuum.idl.api.converter.SpecificC3TypeConverter;
 import org.kinotic.continuum.idl.api.schema.C3Type;
 import org.kinotic.continuum.idl.api.schema.EnumC3Type;
@@ -12,7 +13,7 @@ import java.util.Set;
 /**
  * Created by Navíd Mitchell 🤪 on 6/5/23.
  */
-public class EnumC3TypeToGraphQL implements SpecificC3TypeConverter<GraphQLTypeHolder, EnumC3Type, GraphQLConversionState> {
+public class EnumC3TypeToGraphQL implements SpecificC3TypeConverter<GraphQLTypeHolder, EnumC3Type, GraphQLConversionState>, Cacheable {
 
     private static final Set<Class<? extends C3Type>> supports = Set.of(EnumC3Type.class);
 
@@ -27,7 +28,9 @@ public class EnumC3TypeToGraphQL implements SpecificC3TypeConverter<GraphQLTypeH
             builder.value(value);
         }
 
-        return new GraphQLTypeHolder(builder.build(), builder.build());
+        GraphQLEnumType type = builder.build();
+
+        return new GraphQLTypeHolder(type, type);
     }
 
     @Override
--- structures-core/src/main/java/org/kinotic/structures/internal/idl/converters/graphql/UnionC3TypeToGraphQL.java
+++ structures-core/src/main/java/org/kinotic/structures/internal/idl/converters/graphql/UnionC3TypeToGraphQL.java
@@ -3,6 +3,7 @@ package org.kinotic.structures.internal.idl.converters.graphql;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLUnionType;
 import org.kinotic.continuum.idl.api.converter.C3ConversionContext;
+import org.kinotic.continuum.idl.api.converter.Cacheable;
 import org.kinotic.continuum.idl.api.converter.SpecificC3TypeConverter;
 import org.kinotic.continuum.idl.api.schema.C3Type;
 import org.kinotic.continuum.idl.api.schema.ObjectC3Type;
@@ -14,7 +15,7 @@ import java.util.Set;
 /**
  * Created by Navíd Mitchell 🤪 on 5/27/23.
  */
-public class UnionC3TypeToGraphQL implements SpecificC3TypeConverter<GraphQLTypeHolder, UnionC3Type, GraphQLConversionState>  {
+public class UnionC3TypeToGraphQL implements SpecificC3TypeConverter<GraphQLTypeHolder, UnionC3Type, GraphQLConversionState>, Cacheable {
 
     private static final Set<Class<? extends C3Type>> supports = Set.of(UnionC3Type.class);
 
