f943b7d6d37731c4a44051d1eb1ab0259a478d6b
Navid Mitchell <navid@mitchellsoftware.net>
2024-01-16 00:03:55 -0700

    Added discriminator support for OpenApi

--- structures-core/src/main/java/org/kinotic/structures/internal/idl/converters/openapi/UnionC3TypeToOpenApi.java
+++ structures-core/src/main/java/org/kinotic/structures/internal/idl/converters/openapi/UnionC3TypeToOpenApi.java
@@ -1,5 +1,6 @@
 package org.kinotic.structures.internal.idl.converters.openapi;
 
+import io.swagger.v3.oas.models.media.Discriminator;
 import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
 import org.kinotic.continuum.idl.api.converter.C3ConversionContext;
@@ -8,6 +9,7 @@ import org.kinotic.continuum.idl.api.converter.SpecificC3TypeConverter;
 import org.kinotic.continuum.idl.api.schema.C3Type;
 import org.kinotic.continuum.idl.api.schema.ObjectC3Type;
 import org.kinotic.continuum.idl.api.schema.UnionC3Type;
+import org.kinotic.structures.api.decorators.DiscriminatorDecorator;
 
 import java.util.Set;
 
@@ -24,6 +26,11 @@ public class UnionC3TypeToOpenApi implements SpecificC3TypeConverter<Schema<?>, 
                              C3ConversionContext<Schema<?>, OpenApiConversionState> conversionContext) {
 
         ObjectSchema unionSchema = new ObjectSchema();
+
+        DiscriminatorDecorator discriminatorDecorator = c3Type.findDecorator(DiscriminatorDecorator.class);
+        if(discriminatorDecorator != null && discriminatorDecorator.getPropertyName() != null){
+            unionSchema.setDiscriminator(new Discriminator().propertyName(discriminatorDecorator.getPropertyName()));
+        }
 
         for(ObjectC3Type objectC3Type : c3Type.getTypes()){
 
