12580edfa6c50e324627924fe725567ac26adaf1
Navid Mitchell <navid@mitchellsoftware.net>
2024-12-18 23:56:50 -0700

    Fix for security config

--- structures-core/src/main/java/org/kinotic/structures/internal/config/StructuresConfiguration.java
+++ structures-core/src/main/java/org/kinotic/structures/internal/config/StructuresConfiguration.java
@@ -1,12 +1,17 @@
 package org.kinotic.structures.internal.config;
 
+import io.vertx.core.Vertx;
+import io.vertx.ext.healthchecks.HealthChecks;
+import org.kinotic.structures.api.services.security.AuthorizationServiceFactory;
+import org.kinotic.structures.internal.api.services.impl.security.NoopAuthorizationServiceFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Enables component scanning for structures classes when {@link org.kinotic.continuum.api.annotations.EnableContinuum} is present
- * NOTE: do not define any beans here, since they should not be loaded first necessarily
  * Created by Navíd Mitchell 🤪 on 5/30/23.
  */
 @Configuration
@@ -14,5 +19,15 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "org.kinotic.structures")
 public class StructuresConfiguration {
 
+    @Bean
+    public HealthChecks healthChecks(Vertx vertx){
+        return HealthChecks.create(vertx);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AuthorizationServiceFactory authorizationServiceFactory(){
+        return new NoopAuthorizationServiceFactory();
+    }
 
 }
