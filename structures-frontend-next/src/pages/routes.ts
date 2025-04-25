4d057f84d7662d9006306057b120154af28ed6d7
gormurad <muradyangor303@gmail.com>
2025-04-25 18:52:27 +0400

    sidenav

--- structures-frontend-next/src/pages/routes.ts
+++ structures-frontend-next/src/pages/routes.ts
@@ -2,13 +2,24 @@ import { type RouteMeta, type RouteRecordRaw } from 'vue-router'
 
 const pageRoutes: RouteRecordRaw[] = [
     {
+      path: '/dashboard',
+      component: () => import('@/layouts/MainLayout.vue'),
+      name:"Dashboard",
+      meta: {
+        authenticationRequired: false,
+        showInMainNav: true,
+        icon: 'dashboard.svg',
+        label: 'Dashboard',
+      } as RouteMeta,
+    },
+    {
         path: '/applications',
         component: () => import('@/layouts/MainLayout.vue'),
         name:"Applications",
         meta: {
           authenticationRequired: false,
           showInMainNav: true,
-          icon: 'pi pi-sitemap',
+          icon: 'microchip.svg',
           label: 'Applications',
         } as RouteMeta,
         children: [
@@ -40,16 +51,45 @@ const pageRoutes: RouteRecordRaw[] = [
         ]
     },
     {
-        path: '/application-add',
-        component: () => import('@/pages/NamespaceAddEdit.vue'),
-        // name:"Add",
-        meta: {
-          authenticationRequired: false,
-          showInMainNav: true,
-          icon: 'pi pi-sitemap',
-          label: 'Create Application',
-        } as RouteMeta,
+      path: '/projects',
+      component: () => import('@/layouts/MainLayout.vue'),
+      meta: {
+        authenticationRequired: false,
+        showInMainNav: true,
+        icon: 'folder.svg',
+        label: 'Projects',
+      } as RouteMeta,
+    },
+    {
+      path: '/namespaces',
+      component: () => import('@/layouts/MainLayout.vue'),
+      meta: {
+        authenticationRequired: false,
+        showInMainNav: true,
+        icon: 'objects-column.svg',
+        label: 'Namespaces',
+      } as RouteMeta,
+    },
+    {
+      path: '/settings',
+      component: () => import('@/layouts/MainLayout.vue'),
+      meta: {
+        authenticationRequired: false,
+        showInMainNav: true,
+        icon: 'settings.svg',
+        label: 'Settings',
+      } as RouteMeta,
     }
+    // {
+    //   path: '/login',
+    //   component: () => import('@/pages/Login.vue'),
+    //   meta: {
+    //     authenticationRequired: false,
+    //     showInMainNav: true,
+    //     // icon: 'pi pi-sitemap',
+    //     // label: 'Create Application',
+    //   } as RouteMeta,
+    // }
     // {
     //     path: '/structures', component: () => import('@/frontends/continuum/layouts/NestedLayout.vue'),
     //     meta: {
