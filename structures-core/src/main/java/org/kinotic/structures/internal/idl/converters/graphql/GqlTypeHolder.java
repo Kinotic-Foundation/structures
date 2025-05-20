b16da774b47bef140f964af6cadd594721b0fa61
Navid Mitchell <navid@mitchellsoftware.net>
2025-05-20 02:35:36 -0600

    Removed lombok to work around vscode bug.

--- structures-core/src/main/java/org/kinotic/structures/internal/idl/converters/graphql/GqlTypeHolder.java
+++ structures-core/src/main/java/org/kinotic/structures/internal/idl/converters/graphql/GqlTypeHolder.java
@@ -2,7 +2,6 @@ package org.kinotic.structures.internal.idl.converters.graphql;
 
 import graphql.schema.GraphQLInputType;
 import graphql.schema.GraphQLOutputType;
-import lombok.Builder;
 
 /**
  * Holds the Graph input and output types created for a given C3 type
@@ -11,7 +10,39 @@ import lombok.Builder;
  * @param inputType  The GraphQL input type for the C3 type
  * @param outputType The GraphQL output type for the C3 type
  */
-@Builder(toBuilder = true)
 public record GqlTypeHolder(GraphQLInputType inputType, GraphQLOutputType outputType) {
 
+    public static GqlTypeHolderBuilder builder() {
+        return new GqlTypeHolderBuilder();
+    }
+
+    public GqlTypeHolderBuilder toBuilder() {
+        return new GqlTypeHolderBuilder().inputType(this.inputType).outputType(this.outputType);
+    }
+
+    public static class GqlTypeHolderBuilder {
+        private GraphQLInputType inputType;
+        private GraphQLOutputType outputType;
+
+        GqlTypeHolderBuilder() {
+        }
+
+        public GqlTypeHolder build() {
+            return new GqlTypeHolder(this.inputType, this.outputType);
+        }
+
+        public GqlTypeHolderBuilder inputType(GraphQLInputType inputType) {
+            this.inputType = inputType;
+            return this;
+        }
+
+        public GqlTypeHolderBuilder outputType(GraphQLOutputType outputType) {
+            this.outputType = outputType;
+            return this;
+        }
+
+        public String toString() {
+            return "GqlTypeHolder.GqlTypeHolderBuilder(inputType=" + this.inputType + ", outputType=" + this.outputType + ")";
+        }
+    }
 }
