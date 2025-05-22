a1d1547d9bfcfe0f872748fac294220083f8a89b
gormurad <muradyangor303@gmail.com>
2025-05-22 18:38:16 +0400

    new nuxt project

---
+++ website-nuxt/components/home/HeroComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div
+      class="w-full bg-cover bg-center bg-no-repeat py-1 px-4"
+      style="background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(32, 43, 113, 0.28)), url('/images/background-home.png')"
+
+    >
+    <BaseContainer>
+      <div class="w-full flex justify-center items-center gap-[120px]">
+        <div class="max-w-[550px] mt-16 text-white">
+          <p class="flex flex-col mb-10 leading-[65px]">
+            <span class="text-[81px] font-normal">
+              Data Evolution
+            </span>
+            <span class="text-[66px] font-light text-[#C8FB39]">
+              Made Simple
+            </span>
+          </p>
+          <p class="text-[19px] text-white">
+            Empower your teams to adapt fast—Structures supports safe schema changes,
+            full CRUD operations, and effortless integration through a sleek GUI and OpenAPI.
+          </p>
+          <div class="mt-10 flex gap-6">
+            <button class="px-4 py-2 rounded-lg bg-[#3651ED] text-white font-medium">
+              Start your project
+            </button>
+            <button class="px-4 py-2 rounded-lg bg-white text-slate-500 font-medium">
+              Request for demo
+            </button>
+          </div>
+        </div>
+        <div>
+          <img 
+              src="/images/hero-section-image.png"
+              alt="Hero Section Image"
+              class="max-w-[700px]"
+              />
+        </div>
+      </div>
+    </BaseContainer>
+
+    </div>
+  </template>
+  
