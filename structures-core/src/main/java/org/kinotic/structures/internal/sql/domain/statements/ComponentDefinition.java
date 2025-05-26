6d9d269b0c2a581c1464ad722c595f759878294c
Nicholas Padilla <nicholas@mindsignited.com>
2025-05-26 15:11:20 -0700

    remove unneeded file

--- structures-core/src/main/java/org/kinotic/structures/internal/sql/domain/statements/ComponentDefinition.java
+++
@@ -1,9 +0,0 @@
-package org.kinotic.structures.internal.sql.domain.statements;
-
-/**
- * @param key      e.g., "NUMBER_OF_SHARDS", "NUMBER_OF_REPLICAS", or column name
- * @param value    e.g., "1", "2", or column type
- * @param isColumn True if this is a column definition
- */
-public record ComponentDefinition(String key, String value, boolean isColumn) {
-}
