3a8b90a731d308ec66a0dbb0ca4e9de64ca54787
gormurad <muradyangor303@gmail.com>
2025-05-29 18:48:12 +0400

    graphql changes

---
+++ structures-frontend-next/src/util/useLayout.ts
@@ -0,0 +1,36 @@
+// useLayout.ts
+import dagre from 'dagre'
+import type { Node, Edge } from '@vue-flow/core'
+
+export function useLayout() {
+  function layout(nodes: Node[], edges: Edge[], direction: 'LR' | 'TB' = 'LR'): Node[] {
+    const dagreGraph = new dagre.graphlib.Graph()
+    dagreGraph.setDefaultEdgeLabel(() => ({}))
+    dagreGraph.setGraph({ rankdir: direction })
+
+    nodes.forEach((node) => {
+      dagreGraph.setNode(node.id, {
+        width: node.dimensions?.width || 200,
+        height: node.dimensions?.height || 100,
+      })
+    })
+
+    edges.forEach((edge) => {
+      dagreGraph.setEdge(edge.source, edge.target)
+    })
+
+    dagre.layout(dagreGraph)
+
+    return nodes.map((node) => {
+      const { x, y } = dagreGraph.node(node.id)
+      return {
+        ...node,
+        position: { x, y },
+        // avoid Vue Flow shifting things after layout
+        positionAbsolute: { x, y },
+      }
+    })
+  }
+
+  return { layout }
+}
