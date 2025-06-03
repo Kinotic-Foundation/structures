efe1c77a51701316230f8aedb99cc8c921b14f23
Navid Mitchell <navid@mitchellsoftware.net>
2025-06-03 01:54:03 -0600

    Fixed deployment for website and docs

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true
+  ssr: true,
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/color-mode'],
