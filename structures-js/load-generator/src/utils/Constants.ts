fa93675804b130c0485eaccf80e05e032efc8ed2
Navid Mitchell <navid@mitchellsoftware.net>
2025-06-12 04:04:18 -0600

    More fixes..

--- structures-js/load-generator/src/utils/Constants.ts
+++ structures-js/load-generator/src/utils/Constants.ts
@@ -1,16 +1,16 @@
 import path from 'path'
 
-// In vite-node, __dirname is available
-const baseDir = path.resolve(__dirname, '..')
+// All paths are relative to the project root
+const rootDir = path.resolve(__dirname, '../../')
 
 export class Constants {
-    static readonly ECOMMERCE_ENTITIES_PATH = path.resolve(baseDir, 'entity/domain/ecommerce')
-    static readonly ECOMMERCE_GENERATED_PATH = path.resolve(baseDir, 'services/ecommerce/generated')
-    static readonly ECOMMERCE_DEFINITIONS_PATH = path.resolve(baseDir, '../../dist/entity-definitions')
+    static readonly ECOMMERCE_ENTITIES_PATH = path.resolve(rootDir, 'src/entity/domain/ecommerce')
+    static readonly ECOMMERCE_GENERATED_PATH = path.resolve(rootDir, 'src/services/ecommerce/generated')
+    static readonly ECOMMERCE_DEFINITIONS_PATH = path.resolve(rootDir, 'dist/entity-definitions')
 
     static {
         console.log('Constants paths:')
-        console.log('Base directory:', baseDir)
+        console.log('Root directory:', rootDir)
         console.log('Entities:', this.ECOMMERCE_ENTITIES_PATH)
         console.log('Generated:', this.ECOMMERCE_GENERATED_PATH)
         console.log('Definitions:', this.ECOMMERCE_DEFINITIONS_PATH)
