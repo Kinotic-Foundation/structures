12051edafbf8ac505b63c8c71615792d439b0bc5
NavidMitchell <NavidMitchell@users.noreply.github.com>
2026-09-09 04:07:01 +0000

    deploy: 60f5b525073463ac4d8219b3b0bad8676f5d6f78

--- _nuxt/BKnjVqAV2.js
+++
@@ -1,1 +0,0 @@
-import{On as e,mt as t,qt as n,v as r}from"./B4b-2GOK.js";import{t as i}from"./DOlk8YV-.js";import{t as a}from"./C_NA3Yh4.js";import o from"./B5U84Ism.js";var s={__name:`ProseWarning`,setup(s){let c=i();return(i,s)=>(t(),r(o,{color:`warning`,icon:e(c).ui.icons.warning},{default:n(()=>[a(i.$slots,`default`,{mdcUnwrap:`p`})]),_:3},8,[`icon`]))}};export{s as default};
